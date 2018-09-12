6ce0033e05b0baa3c0da82e2c552e55aac65a306
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2018-09-12 00:26:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function currentLine(line) {
     return `The line is currently empty.`
   } else {
     var array = []
-    for (var = i; i < line.length; i++) {
+    for (var i = 0; i < line.length; i++) {
       array.push(` ${i+1}. ${line[i]}`)
     }
     return `The line is currently:` + array
