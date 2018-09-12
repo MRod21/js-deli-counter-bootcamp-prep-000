e03702f8d49e8909cb6db98b03995c9f06e13aeb
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2018-09-12 00:14:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(katzDeli, name) {
   katzDeli.push(name)
-  return `Wecome, ${name}. You are number ${katzDeli.length} in line.`
+  return `Welcome, ${name}. You are number ${katzDeli.length} in line.`
 }
